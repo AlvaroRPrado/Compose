148f6baf9aa35c4f4bd5903d29a33100ac222683
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2025-02-07 17:14:46 -0300

    Atualizando aula 5.1

--- CursoCompose/app/src/main/java/com/prado/cursocompose/MainActivity.kt
+++ CursoCompose/app/src/main/java/com/prado/cursocompose/MainActivity.kt
@@ -4,15 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text("Hello World")
+            Scaffold { padding ->
+                Box(modifier = Modifier.padding(padding)
+                    .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        text = "Hello World",
+                        color = Color.Red,
+                        fontSize = 34.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
         }
     }
+
 }
 
