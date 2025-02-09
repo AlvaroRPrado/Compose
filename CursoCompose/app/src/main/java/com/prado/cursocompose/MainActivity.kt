2f6a55f3216cfaaa8ef8f662bbcaf5ec3159766b
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2025-02-09 11:53:52 -0300

    Entendo a estrutura de um projeto

--- CursoCompose/app/src/main/java/com/prado/cursocompose/MainActivity.kt
+++ CursoCompose/app/src/main/java/com/prado/cursocompose/MainActivity.kt
@@ -9,31 +9,56 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.prado.cursocompose.ui.theme.BlueText
+import com.prado.cursocompose.ui.theme.CursoComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold { padding ->
-                Box(modifier = Modifier.padding(padding)
-                    .fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ){
-                    Text(
-                        text = "Hello World",
-                        color = Color.Red,
-                        fontSize = 34.sp,
-                        fontWeight = FontWeight.Bold,
+            CursoComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+                   /* Box(
+                        modifier = Modifier.padding(padding)
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                         Text(
+                            text = "Hello World",
+                            color = Color.Red,
+                            fontSize = 34.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                    }*/
+                    Greeting(
+                        name = "Alvaro",
+                        modifier = Modifier.padding(padding)
                     )
                 }
             }
         }
+    }
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier){
+        Text(text = "Hello $name",
+            modifier = modifier,
+            color = BlueText
+        )
+    }
+
+    @Composable
+    @Preview
+    fun GreetingPreview(){
+        Greeting(name = "Alvaro")
     }
 
 }
